fabff6566d115d777cc3d780e27f6c509268c638
jonagr7 <agu14349@uvg.edu.gt>
2015-09-10 20:27:35 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ligia Orantes
+ *
+ */
+public class Nodo <E> {
+	private Association<String, String> p;
+	private Nodo<E> hojad; 
+	private Nodo<E> hojai;
+	
+	public Nodo(Nodo hojad, Nodo hojai, String pi,String pe) {
+		super();
+		this.hojad = hojad;
+		this.hojai = hojai;
+		this.p = new Association<String,String>(pi,pe);
+	}
+	public Nodo<E> getterhd() {
+		return hojad;
+	}
+	public Nodo<E> getterhi() {
+		return hojai;
+	}
+	public String getterpi() {
+		return p.getKey();
+	}
+	public String getterpe() {
+		return p.getValue();
+	}
+	public void setterhd(Nodo hojad) {
+		this.hojad = hojad;
+	}
+	public void setterhi(Nodo hojai) {
+		this.hojai = hojai;
+	}
+	public void setterpi(String pi) {
+		p.setKey(pi);
+	}
+	public void setterpe(String pe) {
+		p.setValue(pe);
+	}
+}
